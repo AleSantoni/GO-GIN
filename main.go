2bde83687593fda655d48a14859fcface796170c
AleSantoni <alesantoni31@gmail.com>
2025-12-22 18:38:45 -0300

    Servidor Basico en Go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		//c.String(200, "¡Hello, World!")
+		c.JSON(200, gin.H{
+			"message": "Hola Mundo",
+		})
+	})
+
+	r.Run(":8080")
+
+}
